1f18d48edd6b4702f6d1a7c8ab4101a8c8af0cea
AnkyXCoder <ankit.5036@gmail.com>
2019-12-30 19:27:29 +0530

    started with a simple calculator program

---
+++ simple_calculator.py
@@ -0,0 +1,24 @@
+global x,y
+def operation(z):
+    print ("You have selected \"",z,"\" operator.\n")
+    if (z == "+"):
+        print (x, " + ", y, " = ", x + y)
+    elif (z == "-"):
+        print (x, " - ", y, " = ", x - y)
+    elif (z == "x"):
+        print (x, " x ", y, " = ", x * y)
+    elif (z == "/"):
+        print (x, " / ", y, " = ", x / y)
+    else :
+        pass
+
+print ("This program does basic mathematical manipulation between two numbers.\n")
+x = int (input("Enter number 1: "))
+print ("Okay, got it")
+y = int (input("Enter number 2: "))
+print ("Okay, got it")
+
+print ("Select operator from the following list:\n+ for addition\n- for subtraction\nx for multiplication\n/ for division\n")
+
+z = input("Waiting for operator: ")
+operation(z)
