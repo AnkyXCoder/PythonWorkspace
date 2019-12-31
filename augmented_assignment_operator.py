82a6089469a0611f3062fbd24f07fa7ab1481ad5
AnkyXCoder <ankit.5036@gmail.com>
2020-01-01 00:31:54 +0530

    augmented assignment operator

---
+++ augmented_assignment_operator.py
@@ -0,0 +1,23 @@
+# Take a random number
+number = 10
+print ("Given value of number is: ", number)
+# Increase it by 4, we can do it by 
+# number = 10 + 4
+# OR by this mwthod
+number = number + 4
+print ("New value of number is: ", number)
+number = number - 8
+print ("New value of number is: ", number)
+number = number * 2
+print ("New value of number is: ", number)
+
+# Augmented Assignement Operator
+print ("Using Augmented Assignement Operator")
+number = 10
+print ("Given value of number is: ", number)
+number += 4
+print ("New value of number is: ", number)
+number -=8
+print ("New value of number is: ", number)
+number *= 2
+print ("New value of number is: ", number)
