af98a0da4f51d6cbfd3f0fbd3cb4ef9562c3c98b
AnkyXCoder <ankit.5036@gmail.com>
2020-01-19 01:12:29 +0530

    demo of list

---
+++ list.py
@@ -0,0 +1,23 @@
+# List
+
+list1 = [1, 2, 3, 4, 5]
+list2 = ['a', 'b', 'c']
+list3 = ["Hello", "Good", "Morning"]
+list4 = [True, False, True, True]
+list5 = [1, 2, 3, 'a', 'b', "Hello", True]
+
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+print(list5)
+
+
+my_inventory = ["sunglasses", "shirts", "t-shirts", "jeans", "watches"]
+print(my_inventory)
+print(my_inventory[0])
+print(my_inventory[1])
+print(my_inventory[2])
+print(my_inventory[3])
+print(my_inventory[4])
+# print(my_inventory[5])
