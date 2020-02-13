ad111aad5aa7ab4598bf0751d9ae9a44ad861015
AnkyXCoder <ankit.5036@gmail.com>
2020-02-13 08:42:57 +0530

    ternary operator

---
+++ python basics II/ternary_operator.py
@@ -0,0 +1,14 @@
+# Ternary Operator
+# Conditional expression
+
+# condition_is_true if condition else condition_if_else
+
+is_friend = True
+can_message = "message allowed" if is_friend else "not allowed to message"
+
+print(can_message)
+
+is_friend = False
+can_message = "message allowed" if is_friend else "not allowed to message"
+
+print(can_message)
