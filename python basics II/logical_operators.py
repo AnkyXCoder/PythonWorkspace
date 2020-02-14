615a9434d116d55b444a0c8649ff8a33bec07720
AnkyXCoder <ankit.5036@gmail.com>
2020-02-14 08:25:53 +0530

    logical operators

---
+++ python basics II/logical_operators.py
@@ -0,0 +1,32 @@
+# Logical Operators
+
+# and   &
+# or    |
+
+# >
+print ("4 > 5", 4 > 5)
+print ("a > b", 'a' > 'b')
+print ('A > a', 'A' > 'a')
+
+# <
+print ("4 < 5", 4 < 5)
+print ("a < b", 'a' < 'b')
+print ('A < a', 'A' < 'a')
+
+# >=
+print("1 >= 0", 1>=0)
+
+# <=
+print("1 <= 0", 1<=0)
+
+# ==
+print("5==5", 5==5)
+
+# !=
+print("5!=5", 5!=5)
+print("5!=4", 5!=4)
+
+
+
+print("1 < 2 < 3 < 4", 1 < 2 < 3 < 4)
+print("1 < 2 > 3 < 4", 1 < 2 > 3 < 4)
