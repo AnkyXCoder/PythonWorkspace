26cbdd48d7dffff4b75d03322a89039b0ffa37b1
Ankit Modi <ankit.modi912@gmail.com>
2021-08-24 08:37:48 +0530

    added OOP_Exercise1

---
+++ Object Oriented Programming/OOP_Exercise1.py
@@ -0,0 +1,21 @@
+#Given the below class:
+class Cat:
+    species = 'mammal'
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+# 1 Instantiate the Cat object with 3 cats
+katty = Cat("Katty", 2)
+tom = Cat("Tom", 3)
+gunny = Cat("Gunny", 4)
+
+
+# 2 Create a function that finds the oldest cat
+def get_oldest_cat(*args):
+    return max(args)
+
+
+# 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
+print(f'The oldest cat is {get_oldest_cat(katty.age, tom.age, gunny.age)} years old.')
